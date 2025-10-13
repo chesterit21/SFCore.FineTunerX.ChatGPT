c2e8e3247ab7a3aeedce873235619f9c6321f563
chesterit21 <chester.it21@gmail.com>
2025-10-13 09:08:23 +0700

    Add initial project structure and empty files for core services and documentation

---
+++ SFCore.FineTuner.Web/Services/PythonBridgeService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SFCore.FineTuner.Web.Services
+{
+    public class PythonBridgeService
+    {
+        
+    }
+}
